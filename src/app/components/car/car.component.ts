5f9b2c4ed955690fafc194eebb675f8718a5b9c6
Igor Nicsenko <igor.nicsenko@fits.hu>
2020-12-23 00:32:03 +0100

    Tutorial 4 add new packeg, and d button

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CarComponent implements OnInit {
 
   model;
-  name:string = 'Audi';
+  name:string;
   speed:number;
   colors:Colors;
   options:string[];
@@ -16,19 +16,51 @@ export class CarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void 
+  ngOnInit(): void
   {
-    this.model = 'bmw';
-    this.name = 'RS8';
-    this.speed = 300;
-    this.colors = {
-      car: 'white',
-      salon: 'Black',
-      wheels: 'Silver'
-    };
-    this.options = ["ABS", "AutoPark", "Sport"];
+  }
+
+  carSelect(carName)
+  {
+    if(carName == 'bmw')
+    {
+      this.model = 'bmw';
+      this.name = 'm3';
+      this.speed = 300;
+      this.colors = {
+        car: 'black',
+        salon: 'Black',
+        wheels: 'black'
+      };
+      this.options = ["sport package", "Manual", "MegaSport"];
+    }
+   else if(carName = 'audi')
+    {
+      this.model = 'audi';
+      this.name = 'RS8';
+      this.speed = 330;
+      this.colors = {
+        car: 'white',
+        salon: 'white',
+        wheels: 'Silver'
+      };
+      this.options = ["midl packeg sport/confort", "Dsg", "LongSport"];
+    }
+   else if(carName = 'merc')
+    {
+      this.model = 'mercedes';
+      this.name = 's330';
+      this.speed = 340;
+      this.colors = {
+        car: 'black',
+        salon: 'white',
+        wheels: 'Silver'
+      };
+      this.options = ["Confort", "Automata", "ConfortSport"];
+    }
   }
 }
+
 
 interface Colors
 {
